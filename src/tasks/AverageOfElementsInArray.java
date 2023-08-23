37402a0e49ae350bbe82f8ce0c0541636b846ace
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2023-08-23 13:19:24 +0300

    Average of elements in array task completed

---
+++ src/tasks/AverageOfElementsInArray.java
@@ -0,0 +1,12 @@
+package tasks;
+
+public class AverageOfElementsInArray {
+    public static void main(String[] args) {
+
+        int[] list = {1, 2, 3, 4, 5};
+        double sum = 0.0;
+        for (int j : list) sum += j;
+        double average = sum / list.length;
+        System.out.println(average);
+    }
+}

094203e18e4caf9aea9ee9c7e4d2e219f6a1581b
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2023-08-23 13:45:33 +0300

    Program to print letter A with multidimensional arrays task completed

---
+++ src/tasks/PrintingLetterAWithMultidimensionalArrays.java
@@ -0,0 +1,27 @@
+package tasks;
+
+public class PrintingLetterAWithMultidimensionalArrays {
+    public static void main(String[] args) {
+
+        String[][] letterA = new String[6][4];
+
+        for (int i = 0; i < letterA.length; i++) {
+            for (int j = 0; j < letterA[i].length; j++) {
+                if (i == 0 || i == 2 || i == 5) {
+                    letterA[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    letterA[i][j] = " * ";
+                } else {
+                    letterA[i][j] = "   ";
+                }
+            }
+        }
+        for (String[] row : letterA) {
+            for (String col : row)
+                System.out.print(col);
+            System.out.println();
+        }
+
+
+    }
+}
